e6f7cb49e9afd1dc1a8a86fbcda08c6e29d3473f
AbhijitPrajapati <talk.abhijitp@gmail.com>
2026-04-09 21:08:41 -0400

    Revert "generate chunks for all methods"
    This reverts commit 7b241d35bfe8c52d5d91df49772c5dd88e3a0ab7.

--- src/multiresolution.py
+++ src/multiresolution.py
@@ -1,12 +1,10 @@
 from nltk import sent_tokenize
 from util.embedding import embedding_model, chunk_sentences_semantically
-from util.vector_store import add_chunks, get_or_init_method
+from util.vector_store import add_chunks
 from util.load_papers import load_papers
 
 
 def main():
-    collection = get_or_init_method("multiresolution")
-
     for data, fn in load_papers():
         chunks = []
         sections = []
@@ -16,7 +14,7 @@ def main():
                 chunks.extend(c)
                 sections.extend([section["section"]] * len(c))
         embeddings = embedding_model.encode(chunks)
-        add_chunks(collection, fn, data["title"], chunks, embeddings, sections)
+        add_chunks("multiresolution", fn, data["title"], chunks, embeddings, sections)
 
 
 if __name__ == "__main__":
